eed6e1ad6bb239807e222bab3afd058c82731caa
Rubayet-billah <rubayetbillah909@gmail.com>
2022-10-27 00:05:44 +0600

    course details added

--- src/Pages/CourseDetails.js
+++ src/Pages/CourseDetails.js
@@ -2,10 +2,23 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const CourseDetails = () => {
-    const { name } = useLoaderData();
+    const { name, img, duration, credit, details, fee } = useLoaderData();
     return (
         <div>
-            {name}
+            <div className='my-8'>
+                <h2 className='text-center text-3xl md:text-6xl'>Here is course details.Just Enroll now and your journey will begin with us.</h2>
+            </div>
+            <div className="card card-compact w-96 mx-auto bg-base-100 shadow-xl">
+                <figure><img src={img} alt="" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <p>{details}</p>
+                    <div className="card-actions justify-between">
+                        <h2 className='text-primary text-4xl font-bold'>${fee}</h2>
+                        <button className="btn btn-primary">Enroll</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/SingleCourse.js
+++ src/Pages/SingleCourse.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SingleCourse = ({ course }) => {
-    const { id } = course;
+    const { id, name, img, details, fee, duration, credit } = course;
     return (
-        <div className=' p-8 m-3 rounded border border-gray-900'>
-            <h2 className='text-4xl'>Single <br />Course</h2>
-            <Link to={`/courses/${id}`}><button className='btn btn-outline'>Get Course</button></Link>
+        <div className='m-3 rounded-lg border border-gray-900'>
+            <img className='rounded-lg' src={img} alt="" />
+            <div className='p-5'>
+                <h2 className='text-2xl my-2'>{name}</h2>
+                <div className='flex justify-between'>
+                    <div>
+                        <p>Credit: {credit}</p>
+                        <p>Duration: {duration}h</p>
+                    </div>
+                    <Link to={`/courses/${id}`}><button className='btn btn-outline'>see details</button></Link>
+                </div>
+            </div>
         </div>
     );
 };
