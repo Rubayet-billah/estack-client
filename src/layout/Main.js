509667620272f6841a732638e8f4a906dd022bf3
Rubayet-billah <rubayetbillah909@gmail.com>
2022-10-27 14:21:29 +0600

    slight change

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -4,13 +4,8 @@ import Header from '../Pages/SharedPages/Header';
 
 const Main = () => {
 
-    const bgImg = {
-        // backgroundImage: "url('https://img.freepik.com/free-vector/network-mesh-wire-digital-technology-background_1017-27428.jpg?w=2000')",
-        backgroundSize: "cover",
-        minHeight: "100vh"
-    }
     return (
-        <div style={bgImg}>
+        <div>
             <Header></Header>
             <section className='container mx-auto'>
                 <Outlet></Outlet>
