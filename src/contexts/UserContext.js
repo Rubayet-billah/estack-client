b3132511088c03e358c4fcf5b9e26d0aa085b55a
Rubayet-billah <rubayetbillah909@gmail.com>
2022-10-27 23:51:42 +0600

    update

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -12,7 +12,7 @@ const UserContext = ({ children }) => {
     // set user in user state
     const [user, setUser] = useState(null);
     // set loading state for handle private route location
-    const [loader, setLoader] = useState(true);
+    const [loader, setLoader] = useState(false);
     // theme toggle state
     const [dark, setDark] = useState(true)
 
